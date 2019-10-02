a1859b5a1412ef1f419bdffc8b984be99a6e8c92
Csaszar Sandor <csaszar@mebedo.eu>
2019-10-02 16:44:16 +0300

    Updates

---
+++ precache-manifest.d42dd50694a919b190d1bc075716f174.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b22e1202bcc9479d963ecbb1b8162b8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7dd4269bb9399ce919aa",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "67f94a2ea62144e7ba6c",
+    "url": "/robofriends/static/css/main.a9cc701e.chunk.css"
+  },
+  {
+    "revision": "7dd4269bb9399ce919aa",
+    "url": "/robofriends/static/js/2.0a6ad8b6.chunk.js"
+  },
+  {
+    "revision": "67f94a2ea62144e7ba6c",
+    "url": "/robofriends/static/js/main.65117c7a.chunk.js"
+  },
+  {
+    "revision": "ef7ed2c3e67d53841831",
+    "url": "/robofriends/static/js/runtime-main.ed74653d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
