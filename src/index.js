a158e1545d2912236394da7c86493a31a6365aae
Csaszar Sandor <csaszar@mebedo.eu>
2019-10-02 16:33:41 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
+import CardList from './components/CardList';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
